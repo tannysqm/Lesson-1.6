94f361d89f8bfbe7921c5334283161b9d3fed9f8
Tanya <t@mail.ru>
2022-12-08 23:26:49 +0300

    Первое задание

---
+++ zdn1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число :");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число :");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine(num1);
+}
+else
+{
+    Console.WriteLine(num2);
+}
