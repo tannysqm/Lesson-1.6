fea5690f344217f954890724b32bbe4d19b1a9e3
Tanya <t@mail.ru>
2022-12-08 23:56:57 +0300

    som save 3

---
+++ zdn1/zdn3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 % 2 == 0)
+{
+    Console.WriteLine("Число чётное");
+}
+
+else 
+{
+    Console.WriteLine("Число нечётное");
